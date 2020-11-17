7640952bb65a6ab3d5c1d6e9b426f3293e6be22f
JuanPablo-Hack <juanpablodejesus_figueroa@ucol.mx>
2020-11-17 11:28:17 -0600

    Cambios de SLG 2.0 2.3

--- SLG/Admin/php/conexion.php
+++ SLG/Admin/php/conexion.php
@@ -3,7 +3,7 @@
 $nombreServidor = "localhost";
 $nombreUsuario = "u298097659_slg";
 $passwordBaseDeDatos = "GrupoSoca1511*";
-$nombreBaseDeDatos = "u298097659_slg|";
+$nombreBaseDeDatos = "u298097659_slg";
 
 $conexion = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
 
