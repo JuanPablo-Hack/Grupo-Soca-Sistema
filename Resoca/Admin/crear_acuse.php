90d69240c76873599365a9d50d63a09afeed9fe3
JuanPablo-Hack <juanpablodejesus_figueroa@ucol.mx>
2020-11-24 15:19:34 -0600

    Cambios de sara2

--- Resoca/Admin/crear_acuse.php
+++ Resoca/Admin/crear_acuse.php
@@ -69,7 +69,7 @@
       <div id="sidebar" class="nav-collapse ">
         <!-- sidebar menu start-->
         <ul class="sidebar-menu" id="nav-accordion">
-          <p class="centered"><a href="profile.html"><img src="img/favicon.ico" class="img-circle" width="80"></a></p>
+          <p class="centered"><a href="profile.html"><img src="img/banner.png" class="img-circle" width="80"></a></p>
           <h5 class="centered">Admin</h5>
           <li class="mt">
             <a class="active" href="index.html">
