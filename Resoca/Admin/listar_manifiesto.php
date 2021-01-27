5f5e39d817095fd9b2e07e52fc7f509dd629c795
JuanPablo-Hack <juanpablodejesus_figueroa@ucol.mx>
2021-01-27 09:16:37 -0600

    Cambios de Aldha

--- Resoca/Admin/listar_manifiesto.php
+++ Resoca/Admin/listar_manifiesto.php
@@ -204,7 +204,8 @@
                     <td><?php echo $mostrar['estado'] ?></td>
                    
                     <td>
-                      <button class="btn btn-success btn-xs"><i class="fa fa-plus-circle"></i></button>
+                      
+                      <a href='./Manifiesto.pdf' class="btn btn-success btn-xs"><i class="fa fa-plus-circle"></i></a>
                       <a href='./editar_manifiesto.php?id=<?php echo $mostrar['id']  ?>' class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
                       <a href='./eliminar_manifiesto.php?id=<?php echo $mostrar['id']  ?>' class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></a>
                       
