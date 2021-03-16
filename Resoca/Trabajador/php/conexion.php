5a9f08b91895a9ab0970cac4a142f5e17fb07cd4
Juan Pablo Hack <juanpablodejesus_figueroa@ucol.mx>
2021-03-16 13:55:30 -0600

    Cambios ultimos antes de beto 3

--- Resoca/Trabajador/php/conexion.php
+++ Resoca/Trabajador/php/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
 $nombreServidor = "localhost";
-$nombreUsuario = "u298097659_resoca";
+$nombreUsuario = "u298097659_rsp";
 $passwordBaseDeDatos = "GrupoSoca1511*";
-$nombreBaseDeDatos = "u298097659_resoca";
+$nombreBaseDeDatos = "u298097659_rsp";
 
 $conexion = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
 
