918bb500efee72316f4aadd062e192a021e09205
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2021-04-02 08:27:08 -0600

    Cambios en el cliente de Resoca

--- Resoca/Cliente/listar_reportes.php
+++ Resoca/Cliente/listar_reportes.php
@@ -68,7 +68,7 @@ session_start();
           <p class="centered"><a href="profile.html"><img src="img/banner.png" class="img-circle" width="80"></a></p>
           <h5 class="centered">Cliente</h5>
           <li class="mt">
-            <a class="active" href="index.html">
+            <a class="active" href="index.php">
               <i class="fa fa-dashboard"></i>
               <span>Panel de Control</span>
             </a>
--- Resoca/Cliente/php/conexion.php
+++ Resoca/Cliente/php/conexion.php
@@ -5,6 +5,8 @@ $nombreUsuario = "u298097659_rsp";
 $passwordBaseDeDatos = "GrupoSoca1511*";
 $nombreBaseDeDatos = "u298097659_rsp";
 
+
+
 $conexion = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
 
 
