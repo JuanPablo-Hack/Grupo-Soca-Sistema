ed8797fde7ed44e52a2aaab8af7c36ae88ae4e0e
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2021-06-14 22:04:23 -0500

    Cambios a produccion 21

--- Soca_Marketers/Ademsa/index.php
+++ Soca_Marketers/Ademsa/index.php
@@ -242,146 +242,7 @@ if ($Row = mysqli_fetch_array($result2)) {
                     <!-- **********************************************************************************************************************************************************
               RIGHT SIDEBAR CONTENT
               *********************************************************************************************************************************************************** -->
-                    <div class="col-lg-3 ds">
-                        <!--COMPLETED ACTIONS DONUTS CHART-->
-                        <div class="donut-main">
-                            <h4>COMPLETED ACTIONS & PROGRESS</h4>
-                            <canvas id="newchart" height="130" width="130"></canvas>
-                            <script>
-                                var doughnutData = [{
-                                    value: 70,
-                                    color: "#4ECDC4"
-                                }, {
-                                    value: 30,
-                                    color: "#fdfdfd"
-                                }];
-                                var myDoughnut = new Chart(document.getElementById("newchart").getContext("2d")).Doughnut(doughnutData);
-                            </script>
-                        </div>
-                        <!--NEW EARNING STATS -->
-                        <div class="panel terques-chart">
-                            <div class="panel-body">
-                                <div class="chart">
-                                    <div class="centered">
-                                        <span>TODAY EARNINGS</span>
-                                        <strong>$ 890,00 | 15%</strong>
-                                    </div>
-                                    <br>
-                                    <div class="sparkline" data-type="line" data-resize="true" data-height="75" data-width="90%" data-line-width="1" data-line-color="#fff" data-spot-color="#fff" data-fill-color="" data-highlight-line-color="#fff" data-spot-radius="4" data-data="[200,135,667,333,526,996,564,123,890,564,455]"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--new earning end-->
-                        <!-- RECENT ACTIVITIES SECTION -->
-                        <h4 class="centered mt">RECENT ACTIVITY</h4>
-                        <!-- First Activity -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <span class="badge bg-theme"><i class="fa fa-clock-o"></i></span>
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <muted>Just Now</muted>
-                                    <br />
-                                    <a href="#">Paul Rudd</a> purchased an item.<br />
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Second Activity -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <span class="badge bg-theme"><i class="fa fa-clock-o"></i></span>
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <muted>2 Minutes Ago</muted>
-                                    <br />
-                                    <a href="#">James Brown</a> subscribed to your newsletter.<br />
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Third Activity -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <span class="badge bg-theme"><i class="fa fa-clock-o"></i></span>
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <muted>3 Hours Ago</muted>
-                                    <br />
-                                    <a href="#">Diana Kennedy</a> purchased a year subscription.<br />
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Fourth Activity -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <span class="badge bg-theme"><i class="fa fa-clock-o"></i></span>
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <muted>7 Hours Ago</muted>
-                                    <br />
-                                    <a href="#">Brando Page</a> purchased a year subscription.<br />
-                                </p>
-                            </div>
-                        </div>
-                        <!-- USERS ONLINE SECTION -->
-                        <h4 class="centered mt">TEAM MEMBERS ONLINE</h4>
-                        <!-- First Member -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <img class="img-circle" src="img/ui-divya.jpg" width="35px" height="35px" align="">
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <a href="#">DIVYA MANIAN</a><br />
-                                    <muted>Available</muted>
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Second Member -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <img class="img-circle" src="img/ui-sherman.jpg" width="35px" height="35px" align="">
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <a href="#">DJ SHERMAN</a><br />
-                                    <muted>I am Busy</muted>
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Third Member -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <img class="img-circle" src="img/ui-danro.jpg" width="35px" height="35px" align="">
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <a href="#">DAN ROGERS</a><br />
-                                    <muted>Available</muted>
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Fourth Member -->
-                        <div class="desc">
-                            <div class="thumb">
-                                <img class="img-circle" src="img/ui-zac.jpg" width="35px" height="35px" align="">
-                            </div>
-                            <div class="details">
-                                <p>
-                                    <a href="#">Zac Sniders</a><br />
-                                    <muted>Available</muted>
-                                </p>
-                            </div>
-                        </div>
-                        <!-- CALENDAR-->
-
-                        <!-- / calendar -->
-                    </div>
-                    <!-- /col-lg-3 -->
-                </div>
+                   
                 <!-- /row -->
             </section>
         </section>
@@ -427,9 +288,9 @@ if ($Row = mysqli_fetch_array($result2)) {
         $(document).ready(function() {
             var unique_id = $.gritter.add({
                 // (string | mandatory) the heading of the notification
-                title: 'Bienvenido Administrador',
+                title: 'Bienvenido Ademsa',
                 // (string | mandatory) the text inside the notification
-                text: 'Recuerda de revisar todos tus pendientes para la semana, éxito!.',
+                text: 'Usa el sistema SOCA MARkETERS para información relevante!.',
                 // (string | optional) the image to display on the left
                 image: 'img/banner.png',
                 // (bool | optional) if you want it to fade out on its own or just sit there
