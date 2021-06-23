7e9877c7615d941d7f19281a5f0b440a11e19930
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2021-06-23 13:05:09 -0500

    Cambios a produccion 11

--- Soca_Marketers/Trabajador/listar_compra.php
+++ Soca_Marketers/Trabajador/listar_compra.php
@@ -223,7 +223,7 @@
                     <td>
                      
                       
-                      <a href='../patio/<?php echo $mostrar['no_guia'] . "/".$mostrar['ruta']?>'  target="_blank" class="btn btn-primary btn-xs"><i class="fa fa-eye"></i></a>
+                      <a href='../patio/<?php echo $mostrar['no_guia'] . "/".$mostrar['foto']?>'  target="_blank" class="btn btn-primary btn-xs"><i class="fa fa-eye"></i></a>
                       <a onclick="crearPDF(<?php echo $mostrar['id'] ?>)" class="btn btn-success btn-xs"><i class="fa fa-plus-circle"></i></a>
                       <a href='./editar_orden2.php?id=<?php echo $mostrar['id']  ?>' class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
                       <a onclick="deleteUser(<?php echo $mostrar['id'] ?>)" class="btn btn-danger btn-xs"> <i class="fa fa-trash-o "></i></a>
