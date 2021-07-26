b24ba8cf0822bf794df8f03703b3d252294501e3
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2021-07-25 20:04:04 -0500

    Cambios en el registro de ingreso a patio

--- Soca_Marketers/Trabajador/crear_orden2.php
+++ Soca_Marketers/Trabajador/crear_orden2.php
@@ -334,7 +334,7 @@ $result5 = mysqli_query($conexion, $sql5);
                       <?php
                       while ($Row1 = mysqli_fetch_array($result5)) {
                       ?>
-                        <option value=<?php echo $Row1['id']; ?>><?php echo $Row1['placas']; ?></option>
+                        <option value=<?php echo $Row1['id_trans']; ?>><?php echo $Row1['placas']; ?></option>
                       <?php
                       }
                       ?>
